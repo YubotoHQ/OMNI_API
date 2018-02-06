aad99f3675585bf95eb29b2be7de309816e06992
mkarathanos <36189104+mkarathanos@users.noreply.github.com>
2018-02-06 13:12:37 +0200

    Create balance-request_javascript-jQuery-request.js

---
+++ balance-request_javascript-jQuery-request.js
@@ -0,0 +1,16 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://services.yuboto.com/omni/v1/Balance",
+  "method": "POST",
+  "headers": {
+    "Content-Type": "application/json; charset=utf-8",
+    "Authorization": "Basic QUZCNUY3RjQtNkZBNS00QTcxLTkxRjktNjBBNDg1RDQ3QTk2",
+    "Cache-Control": "no-cache",
+    "Postman-Token": "e4785b45-7f9a-c0a2-b000-83445a54b355"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
