4a75e8439a1c7a8185cd1022b1db47a16f69ed3c
mkarathanos <36189104+mkarathanos@users.noreply.github.com>
2018-02-06 13:11:57 +0200

    Update balance-request_javascript-XHR-request.js

--- balance-request_javascript-XHR-request.js
+++ balance-request_javascript-XHR-request.js
@@ -13,6 +13,5 @@ xhr.open("POST", "https://services.yuboto.com/omni/v1/Balance");
 xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 xhr.setRequestHeader("Authorization", "Basic QUZCNUY3RjQtNkZBNS00QTcxLTkxRjktNjBBNDg1RDQ3QTk2");
 xhr.setRequestHeader("Cache-Control", "no-cache");
-xhr.setRequestHeader("Postman-Token", "5f7b0b7c-da3c-a381-a99c-20e8116a9b25");
 
 xhr.send(data);
