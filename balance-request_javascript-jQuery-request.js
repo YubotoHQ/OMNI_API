3f37d7b980be0e34c6486e6e3c4466b97637ff23
mkarathanos <36189104+mkarathanos@users.noreply.github.com>
2018-02-07 13:13:28 +0200

    Update balance-request_javascript-jQuery-request.js

--- balance-request_javascript-jQuery-request.js
+++ balance-request_javascript-jQuery-request.js
@@ -5,7 +5,7 @@ var settings = {
   "method": "POST",
   "headers": {
     "Content-Type": "application/json; charset=utf-8",
-    "Authorization": "Basic QUZCNUY3RjQtNkZBNS00QTcxLTkxRjktNjBBNDg1RDQ3QTk2",
+    "Authorization": "Basic {Base64 Encoded API KEY}",
     "Cache-Control": "no-cache",
     "Postman-Token": "e4785b45-7f9a-c0a2-b000-83445a54b355"
   }
